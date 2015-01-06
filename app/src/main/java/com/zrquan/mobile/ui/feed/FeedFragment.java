c1375c2977f56745fdc9a8253584014a67c36216
vicky <yuqi.fan@foxmail.com>
2015-01-06 13:54:18 +0800

    首页 代码构建

--- app/src/main/java/com/zrquan/mobile/ui/feed/FeedFragment.java
+++ app/src/main/java/com/zrquan/mobile/ui/feed/FeedFragment.java
@@ -10,7 +10,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.zrquan.mobile.R;
 
@@ -33,7 +35,10 @@ public class FeedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.activity_feed, container, false);
+        View view = inflater.inflate(R.layout.fragment_feed, container, false);
+        initViewPager(view);
+        initNavBar(view);
+        return view;
     }
 
     @Override
@@ -59,5 +64,74 @@ public class FeedFragment extends Fragment {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /*
+	 * 初始化导航栏
+	 */
+    private void initNavBar(View view){
+        tvDiscussion = (TextView)view.findViewById(R.id.tv_discussion);
+        tvQuestion = (TextView)view.findViewById(R.id.tv_question);
+
+        tvDiscussion.setOnClickListener(new TxListener(0));
+        tvQuestion.setOnClickListener(new TxListener(1));
+    }
+
+    private class TxListener implements View.OnClickListener{
+        private int index=0;
+
+        public TxListener(int i) {
+            index =i;
+        }
+        @Override
+        public void onClick(View v) {
+            mPager.setCurrentItem(index);
+        }
+    }
+
+    /*
+     * 初始化ViewPager
+     */
+    private void initViewPager(View view){
+        mPager = (ViewPager) view.findViewById(R.id.viewpager);
+        fragmentList = new ArrayList<>();
+        fragmentList.add(new DiscussionFragment());
+        fragmentList.add(new QuestionFragment());
+
+        //给ViewPager设置适配器
+        mPager.setAdapter(new FeedPagerAdapter(getChildFragmentManager(), fragmentList));
+        mPager.setCurrentItem(0);                                           //设置当前显示标签页为第一页
+        mPager.setOnPageChangeListener(new MyOnPageChangeListener());       //页面变化时的监听器
+    }
+
+    private class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+            // 取得该控件的实例
+            LinearLayout.LayoutParams ll = (android.widget.LinearLayout.LayoutParams) barText
+                    .getLayoutParams();
+
+            if(currIndex == arg0){
+                ll.leftMargin = (int) (currIndex * barText.getWidth() + arg1
+                        * barText.getWidth());
+            }else if(currIndex > arg0){
+                ll.leftMargin = (int) (currIndex * barText.getWidth() - (1 - arg1)* barText.getWidth());
+            }
+            barText.setLayoutParams(ll);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onPageSelected(int arg0) {
+            currIndex = arg0;
+            int i = currIndex + 1;
+            Toast.makeText(getActivity(), "您选择了第" + i + "个页卡", Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/zrquan/mobile/ui/feed/FeedPagerAdapter.java
@@ -0,0 +1,28 @@
+package com.zrquan.mobile.ui.feed;
+
+import java.util.ArrayList;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class FeedPagerAdapter extends FragmentPagerAdapter{
+
+    ArrayList<Fragment> list;
+    public FeedPagerAdapter(FragmentManager fm,ArrayList<Fragment> list) {
+        super(fm);
+        this.list = list;
+
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Fragment getItem(int arg0) {
+        return list.get(arg0);
+    }
+
+}
