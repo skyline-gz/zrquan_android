8bd529605b12f0aef66da1c0eaab572676616c1a
vicky <yuqi.fan@foxmail.com>
2015-01-08 16:18:59 +0800

    整理资源 dialog test

--- app/src/main/java/com/zrquan/mobile/ui/feed/QuestionFragment.java
+++ app/src/main/java/com/zrquan/mobile/ui/feed/QuestionFragment.java
@@ -1,20 +1,50 @@
 package com.zrquan.mobile.ui.feed;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.zrquan.mobile.R;
 
 //问答 动态
 public class QuestionFragment extends Fragment {
-    
+
+    private TextView tvTest;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_question, null);
+        View v = inflater.inflate(R.layout.fragment_question, null);
+        tvTest = (TextView) v.findViewById(R.id.tv_test);
+        tvTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDialog();
+            }
+        });
+        return v;
+    }
+
+    public void createDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+//                        MyActivity.this.finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        builder.show();
     }
 }
