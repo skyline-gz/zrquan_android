d0a67be1447335b2ac06f9a2ff50abd522d7e869
vicky <yuqi.fan@foxmail.com>
2015-01-15 15:46:07 +0800

    增加splash

---
+++ app/src/main/java/com/zrquan/mobile/ui/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.zrquan.mobile.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.zrquan.mobile.R;
+
+public class SplashActivity extends Activity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+                overridePendingTransition(R.anim.main_enter, R.anim.main_exit );
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
