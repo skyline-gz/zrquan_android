e61a6723720c8334a66df12f085ed6a1d33de5ea
vicky <yuqi.fan@foxmail.com>
2015-01-22 14:03:37 +0800

    注册页面交互，代码构建

--- app/src/main/java/com/zrquan/mobile/support/util/RegUtil.java
+++ app/src/main/java/com/zrquan/mobile/support/util/RegUtil.java
@@ -7,7 +7,9 @@ public class RegUtil {
 
     private Pattern emailPattern = Pattern.compile("(([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)*\\.([a-zA-Z0-9_\\-\\.])+)");
     private Pattern ipPattern = Pattern.compile("((http|ftp|https):\\/\\/((25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d)))\\.){3}(?:25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d))))");
-    private Pattern phonePattern = Pattern.compile("(?<!\\d)((((\\+86)|(86))?(1[3|4|5|7|8])\\d{9})|^((\\d{7,8})|(\\d{4}|\\d{3})-(\\d{7,8})|(\\d{4}|\\d{3})-(\\d{7,8})-(\\d{4}|\\d{3}|\\d{2}|\\d{1})|(\\d{7,8})-(\\d{4}|\\d{3}|\\d{2}|\\d{1})$))(?!\\d)");
+//    private Pattern phonePattern = Pattern.compile("(?<!\\d)((((\\+86)|(86))?(1[3|4|5|7|8])\\d{9})|^((\\d{7,8})|(\\d{4}|\\d{3})-(\\d{7,8})|(\\d{4}|\\d{3})-(\\d{7,8})-(\\d{4}|\\d{3}|\\d{2}|\\d{1})|(\\d{7,8})-(\\d{4}|\\d{3}|\\d{2}|\\d{1})$))(?!\\d)");
+    private Pattern mobilePattern = Pattern.compile("(1\\d{10})");
+    private Pattern passwordPattern = Pattern.compile("([a-zA-Z0-9]{8,20})");
     private Pattern urlPattern = Pattern.compile("(([Hh][Tt][Tt][Pp]://)|([Hh][Tt][Tt][Pp][Ss]://))?(((([a-zA-Z0-9_\\-])+[.]){1,}([a-zA-Z0-9\\-]+)(((\\/[\\~]*|\\[\\~]*)([a-zA-Z0-9_\\-])+)|[.]([a-zA-Z0-9_\\-])+)*(((([?|#]([a-zA-Z0-9_\\-])+){1}[=]*))*(([a-zA-Z0-9_\\-])+){1}([\\&]*([a-zA-Z0-9_\\-])*[\\=]*([a-zA-Z0-9_\\-])*[\\%]*([a-zA-Z0-9_\\-])*)*)*))|(www){1}([[a-zA-Z0-9_\\-]\\.\\-/:]+)([a-zA-Z0-9\\-]+)");
 
     public static RegUtil getInstance() {
@@ -22,8 +24,11 @@ public class RegUtil {
         return this.ipPattern;
     }
 
-    public Pattern getPhonePattern() {
-        return this.phonePattern;
+    public Pattern getMobilePattern() {
+        return this.mobilePattern;
+    }
+    public Pattern getPasswordPattern() {
+        return this.passwordPattern;
     }
 
     public Pattern getUrlPattern() {
--- app/src/main/java/com/zrquan/mobile/ui/UserRegisterActivity.java
+++ app/src/main/java/com/zrquan/mobile/ui/UserRegisterActivity.java
@@ -6,19 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.zrquan.mobile.R;
+import com.zrquan.mobile.support.util.RegUtil;
+
+import java.util.regex.Matcher;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import butterknife.OnTextChanged;
 
 public class UserRegisterActivity extends Activity{
     private Context context;
 
     @InjectView(R.id.titleText) TextView tvTitle;
-    @InjectView(R.id.tv_btn_back) TextView tvBtnBack;
+    @InjectView(R.id.tv_btn_back) TextView tvBackBtn;
+    @InjectView(R.id.tv_tips) TextView tvInputTips;
+    @InjectView(R.id.btRegist) Button btRegist;
+    @InjectView(R.id.phoneNum) EditText etPhoneNum;
+    @InjectView(R.id.regist_phone_num_clear_btn) ImageView ivClearPhoneNumBtn;
+    @InjectView(R.id.password) EditText etPassword;
+    @InjectView(R.id.regist_password_clear_btn) ImageView ivClearPasswordBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +57,79 @@ public class UserRegisterActivity extends Activity{
     private void initNavigationBar() {
         tvTitle.setText(R.string.account_regist);
         tvTitle.setVisibility(View.VISIBLE);
-        tvBtnBack.setVisibility(View.VISIBLE);
+        tvBackBtn.setVisibility(View.VISIBLE);
+    }
+
+    @OnClick(R.id.btRegist)
+    public void onRegisterBtnClick() {
+        Toast.makeText(this, "点击了登陆按钮", Toast.LENGTH_LONG).show();
+    }
+
+    @OnTextChanged(R.id.phoneNum)
+    public void onPhoneNumTextChanged(CharSequence s, int start, int before, int count) {
+        if(s.toString().trim().length() > 0) {
+            ivClearPhoneNumBtn.setVisibility(View.VISIBLE);
+        } else {
+            ivClearPhoneNumBtn.setVisibility(View.GONE);
+        }
+        checkRegisterParams();
+    }
+
+    @OnTextChanged(R.id.password)
+    public void onPasswordTextChanged(CharSequence s, int start, int before, int count) {
+        if(s.toString().trim().length() > 0) {
+            ivClearPasswordBtn.setVisibility(View.VISIBLE);
+        } else {
+            ivClearPasswordBtn.setVisibility(View.GONE);
+        }
+        checkRegisterParams();
+    }
+
+    @OnClick(R.id.regist_phone_num_clear_btn)
+    public void onClearPhoneNumBtnClick(View v) {
+        etPhoneNum.setText("");
+    }
+
+    @OnClick(R.id.regist_phone_num_clear_btn)
+    public void regist_password_clear_btn(View v) {
+        etPassword.setText("");
+    }
+
+    private void checkRegisterParams() {
+        if(checkPhoneNum(etPhoneNum.getText().toString())
+                && checkPassword(etPassword.getText().toString())) {
+            btRegist.setEnabled(true);
+            btRegist.setTextColor(getResources().getColor(R.color.main_button_shadow_text_color_for_light_color_button));
+        } else {
+            btRegist.setEnabled(false);
+            btRegist.setTextColor(getResources().getColor(R.color.main_button_disabled_text_color_for_light_color_button));
+        }
+    }
+
+    private boolean checkPhoneNum(String s) {
+        Matcher matcher = RegUtil.getInstance().getMobilePattern().matcher(s);
+        if(!matcher.matches()) {
+            tvInputTips.setText(R.string.login_phone_number_error_tips);
+            tvInputTips.setTextColor(getResources().getColor(R.color.main_highlight_text_color));
+            return false;
+        } else {
+            tvInputTips.setText(R.string.pwd_tips);
+            tvInputTips.setTextColor(getResources().getColor(R.color.main_content_subtitle_text_color));
+        }
+        return true;
+    }
+
+    private boolean checkPassword(String s) {
+        Matcher matcher = RegUtil.getInstance().getPasswordPattern().matcher(s);
+        if(!matcher.matches()) {
+            tvInputTips.setText(R.string.pwd_tips);
+            tvInputTips.setTextColor(getResources().getColor(R.color.main_highlight_text_color));
+            return false;
+        } else {
+            tvInputTips.setText(R.string.pwd_tips);
+            tvInputTips.setTextColor(getResources().getColor(R.color.main_content_subtitle_text_color));
+        }
+        return true;
     }
 
     private void doBack() {
