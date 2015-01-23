21ee056f769c657071434ec9f5db1be2329a3863
vicky <yuqi.fan@foxmail.com>
2015-01-23 19:20:52 +0800

    写作窗口，代码构建

--- app/src/main/java/com/zrquan/mobile/ui/MainActivity.java
+++ app/src/main/java/com/zrquan/mobile/ui/MainActivity.java
@@ -1,11 +1,14 @@
 package com.zrquan.mobile.ui;
 
+import android.content.Context;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTabHost;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,14 +16,16 @@ import android.widget.Toast;
 import com.google.common.base.Optional;
 import com.zrquan.mobile.R;
 import com.zrquan.mobile.ui.feed.FeedFragment;
+import com.zrquan.mobile.widget.fragment.FragmentTabHost;
 
 public class MainActivity extends FragmentActivity {
     private LayoutInflater inflater;
+    private Context context;
 
     private final int COMPOSE_TAB_INDEX = 2;
 
     //定义数组来存放Fragment界面
-    private Class fragmentArray[] = {FeedFragment.class, FollowFragment.class, FollowFragment.class,
+    private Class fragmentArray[] = {FeedFragment.class, FollowFragment.class, null,
             MessageFragment.class, ProfileFragment.class};
 
     //定义数组来存放按钮图片
@@ -30,11 +35,14 @@ public class MainActivity extends FragmentActivity {
     //Tab选项卡的文字
     private String mTextViewArray[] = {"首页", "关注的", "发表", "消息", "我"};
 
+    private PopupWindow mPopupWindow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        context = getApplicationContext();
         inflater = LayoutInflater.from(this);
         FragmentTabHost mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
         mTabHost.setup(getApplicationContext(), getSupportFragmentManager(), R.id.real_tab_content);
@@ -45,14 +53,15 @@ public class MainActivity extends FragmentActivity {
         for (int i = 0; i < count; i++) {
 
             View indicator;
+            TabHost.TabSpec tabSpec;
             if(i == COMPOSE_TAB_INDEX) {
+                //设置写作Tab
                 indicator = inflater.inflate(R.layout.tab_compose, null);
             } else {
+                //为每一个Tab按钮设置图标、文字和内容
                 indicator = getIndicatorView(mTextViewArray[i], mImageViewArray[i]);
             }
-
-            //为每一个Tab按钮设置图标、文字和内容
-            TabHost.TabSpec tabSpec = mTabHost.newTabSpec(mTextViewArray[i]).setIndicator(indicator);
+            tabSpec = mTabHost.newTabSpec(mTextViewArray[i]).setIndicator(indicator);
             //将Tab按钮添加进Tab选项卡中
             mTabHost.addTab(tabSpec, fragmentArray[i], null);
             //设置Tab按钮的背景
@@ -65,6 +74,29 @@ public class MainActivity extends FragmentActivity {
         mTabHost.getTabWidget().setStripEnabled(false);
         //去掉tab icon之间的间隔线
         mTabHost.getTabWidget().setDividerDrawable(null);
+        //当点击发表按钮时，弹出发表按钮的popupwindow,由于此处监听的是TabChange,因此当收回popupWindow时需要select之前的View
+        mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                if(tabId.equals(mTextViewArray[COMPOSE_TAB_INDEX])) {
+                    mPopupWindow.setAnimationStyle(R.style.ComposePopupAnimation);
+                    mPopupWindow.showAtLocation(findViewById(android.R.id.content), Gravity.TOP| Gravity.START, 0, 0);
+                    mPopupWindow.update();
+                }
+            }
+        });
+
+        initComposeWindow();
+    }
+
+    private void initComposeWindow() {
+        View popupView = getLayoutInflater().inflate(R.layout.pop_up_window_compose, null);
+
+        mPopupWindow = new PopupWindow(popupView, WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.MATCH_PARENT, true);
+        mPopupWindow.setTouchable(true);
+        mPopupWindow.setOutsideTouchable(true);
+//        mPopupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
     }
 
 //    @Override
