99d1093dd0981c52c0130f77889f2f20e8e20eb2
vicky <yuqi.fan@foxmail.com>
2015-02-05 19:36:15 +0800

    图片预览，支持gif

--- app/src/main/java/com/zrquan/mobile/ui/demo/GalleryViewPagerSampleActivity.java
+++ app/src/main/java/com/zrquan/mobile/ui/demo/GalleryViewPagerSampleActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import com.viewpagerindicator.CirclePageIndicator;
@@ -16,6 +17,8 @@ import com.zrquan.mobile.widget.gallery.TileBitmapDrawable;
 import com.zrquan.mobile.widget.gallery.TouchImageView;
 
 import java.io.InputStream;
+
+import pl.droidsonroids.gif.GifDrawable;
 
 public class GalleryViewPagerSampleActivity extends FragmentActivity {
 	
@@ -35,7 +38,8 @@ public class GalleryViewPagerSampleActivity extends FragmentActivity {
 	
 	private final class GalleryAdapter extends FragmentStatePagerAdapter {
 		
-		private int[] images = {R.raw.photo1, R.raw.photo2, R.raw.photo3, R.raw.android1, R.raw.android2, R.raw.android3};
+		private int[] images = { R.raw.gif_runing_hourse, R.raw.photo1, R.raw.photo2, R.raw.photo3,
+                R.raw.android1, R.raw.android2, R.raw.android3};
 		
 		GalleryAdapter() {
 			super(getSupportFragmentManager());
@@ -51,28 +55,41 @@ public class GalleryViewPagerSampleActivity extends FragmentActivity {
 			return GalleryFragment.getInstance(this.images[position]);
 		}
 	}
-	
+
 	public static final class GalleryFragment extends Fragment {
-		
+
 		public static GalleryFragment getInstance(int imageId) {
 			final GalleryFragment instance = new GalleryFragment();
 			final Bundle params = new Bundle();
 			params.putInt("imageId", imageId);
 			instance.setArguments(params);
-			
+
 			return instance;
 		}
-		
+
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 			final View v = inflater.inflate(R.layout.gallery_view_pager_sample_item, null);
-			
+
 			final TouchImageView image = (TouchImageView) v.findViewById(R.id.gallery_view_pager_sample_item_image);
 			final int imageId = getArguments().getInt("imageId");
-			final InputStream is = getResources().openRawResource(imageId);
-			
+
 			final ProgressBar progress = (ProgressBar) v.findViewById(R.id.gallery_view_pager_sample_item_progress);
-			
+
+            //根据文件名渲染GIF
+            if(v.getResources().getResourceEntryName(imageId).toLowerCase().startsWith("gif")) {
+                try{
+                    image.setImageDrawable(new GifDrawable(v.getResources(), imageId));
+                    progress.setVisibility(View.GONE);
+                } catch (Exception e) {
+
+                }
+                return v;
+            }
+
+            //渲染其它Bitmap
+			final InputStream is = getResources().openRawResource(imageId);
+
 			TileBitmapDrawable.attachTileBitmapDrawable(image, is, null, new TileBitmapDrawable.OnInitializeListener() {
 
                 @Override
@@ -90,9 +107,9 @@ public class GalleryViewPagerSampleActivity extends FragmentActivity {
 
                 }
             });
-			
+
 			return v;
 		}
-		
+
 	}
 }
